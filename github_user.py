d90b4c55ce2e5ac4ac96ff102b0f5e2e82685aba
Divyesh Prajapti <divyeshprajapati.a@gmail.com>
2026-01-02 23:54:08 +0530

    Add Github user info CLI using Github API

--- github_user.py
+++ github_user.py
@@ -1,4 +1,6 @@
+import sys
 import requests
+import argparse
 
 def fatch_github_user(username):
     url = f'http://api.github.com/users/{username}'
@@ -18,15 +20,34 @@ def extrect_user_info(data):
         "Public Repos":data.get("public_repos","Not available"),
         "Followers":data.get("followers","Not available"),
         "Following":data.get("following","Not available"),
-        "Account Created":data.get("created_at","Not available"),
+        "Account Created":data.get("created_at","Not available")
     }
+def main():
+    parser = argparse.ArgumentParser(
+        description="Github User Info CLI"
+    )
+
+    parser.add_argument(
+        "username",
+        help="Github username to fatch"
+    )
+    args = parser.parse_args()
+
+    try:
+        raw_data = fatch_github_user(args.username)
+        if raw_data is None:
+            print(f'user {args.username} is not found')
+            sys.exit(1)
+        
+        user_info = extrect_user_info(raw_data)
+        print("\nGithub User Info")
+        print("-------------------------")
+        for key,value in user_info.items(): 
+            print(f"{key} : {value}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        sys.exit(1)
     
 if __name__ == "__main__":
-    row_data = fatch_github_user("divyeshprajapatia-dev")
-
-    if row_data == None:
-        print("user not found")
-    else:
-        user_info = extrect_user_info(row_data)
-        print(user_info)
+    main()
 
